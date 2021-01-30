cf1bec3c46786343d1bbce530bc09f788ca76582
ron <759782075@qq.com>
2021-01-30 12:29:52 +0800

    21/1/30

--- example-project/example/settings.py
+++ example-project/example/settings.py
@@ -10,8 +10,11 @@ NEWSPIDER_MODULE = 'example.spiders'
 
 USER_AGENT = 'scrapy-redis (+https://github.com/rolando/scrapy-redis)'
 
+#设置重复过滤器的模块
 DUPEFILTER_CLASS = "scrapy_redis.dupefilter.RFPDupeFilter"
+#设置调度器，scrapy_redis的调度器具备与数据库交互的功能
 SCHEDULER = "scrapy_redis.scheduler.Scheduler"
+#设置当爬虫结束的时候是否保持redis数据库中的去重集合与任务队列
 SCHEDULER_PERSIST = True
 #SCHEDULER_QUEUE_CLASS = "scrapy_redis.queue.SpiderPriorityQueue"
 #SCHEDULER_QUEUE_CLASS = "scrapy_redis.queue.SpiderQueue"
@@ -19,9 +22,11 @@ SCHEDULER_PERSIST = True
 
 ITEM_PIPELINES = {
     'example.pipelines.ExamplePipeline': 300,
+    #当开启该管道，该管道将会把数据存到redis数据库中
     'scrapy_redis.pipelines.RedisPipeline': 400,
 }
-
+#设置redis数据库
+REDIS_URL='redis://127.0.0.1:6379'
 LOG_LEVEL = 'DEBUG'
 
 # Introduce an artifical delay to make use of parallelism. to speed up the
